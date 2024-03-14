0f3d72a12c4e8b4df96bd4a3867d8bcb0cb9fc21
hshziwo <hshziwo@gmail.com>
2024-03-15 00:27:13 +0900

    이코테 greedy 복습완료

--- greedy/이코테/모험가 길드 복습.py
+++ greedy/이코테/모험가 길드 복습.py
@@ -1,17 +1,15 @@
 n = int(input())
 array = list(map(int, input().split()))
 
-array = sorted(array)
 count = 0
+array = sorted(array)
 index = 0
-while True :
-    next_index = index + array[index]
-    if next_index < len(array) and array[index] == array[next_index] :
+while True:
+    next_index = index + array[index] - 1
+    if next_index < len(array) and array[index] == array[next_index]:
         count += 1
         index = next_index + 1
-    # else 
-
-    if next_index > len(array) :
+    else:
         break
 
 print(count)
